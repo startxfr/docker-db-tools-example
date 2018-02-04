82628589abdb218a43edcf9b0982135f8628fa42
startxfr <clarue@startx.fr>
2018-02-04 01:28:15 +0100

    add example file structure

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # docker-db-tools-example
+
 Example application for the docker-db-tools project
+
+For more information please refer to [sx-dbtools mainpage](https://github.com/startxfr/docker-db-tools)
---
+++ mysql/example.data.sql
@@ -0,0 +1,31 @@
+SET names 'utf8';
+-- MySQL dump 10.14  Distrib 5.5.58-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: 172.17.0.99    Database: demo
+-- ------------------------------------------------------
+-- Server version	5.5.59-MariaDB-1~wheezy
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `app`
+--
+
+LOCK TABLES `app` WRITE;
+INSERT INTO `app` VALUES 
+(1,'version','0.1.11');
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'demo'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-02-02 23:10:06
